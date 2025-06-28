2d37acbb271a20a4b7a2353c875b7525cccb892f
kep2711 <kevinciang27@gmail.com>
2025-06-28 12:35:55 +0800

    Add files via upload

---
+++ prompt.js
@@ -0,0 +1,8 @@
+// let username =prompt('Heyhey isi nama kamu dongg🤩🤩')
+// let age=Number(prompt('kamu sekarang umur berapa nich??🤔🤔'))
+// if(username && age){
+//     alert(`🎉 Happy birthdayyy ${username}! 🎂 Semoga di umur ke-${age} kamu makin sukses dan bahagia! 🥳(dan tetep bersama orang yang sama ya💟😜)`)
+// }else{
+//     alert('isi biodata kamu dongg pwiss😌😌')
+// }
+
